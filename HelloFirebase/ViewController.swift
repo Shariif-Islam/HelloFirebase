b7c59c0e085df4fbaf85f3aa787390da66eddd2c
Shariif Islam <islamshariif@gmail.com>
2017-07-19 13:05:43 +0600

    push main project

--- HelloFirebase/ViewController.swift
+++ HelloFirebase/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lb_condition: UILabel!
+    
+    // create the reference of database with a path
+    let rootRef = Database.database().reference().child("condition")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        rootRef.observe(.value) { (dsnap : DataSnapshot) in
+            self.lb_condition.text = dsnap.value as? String
+            self.changeBackgroundColor(text: (dsnap.value as? String)!)
+        }
+    }
+    
+    func changeBackgroundColor(text: String) {
+        
+        if text == "sunny" {
+            view.backgroundColor = UIColor.white
+        }
+        else {
+            view.backgroundColor = UIColor.lightGray
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sunny(_ sender: Any) {
+        rootRef.setValue("sunny")
+        changeBackgroundColor(text: "sunny")
     }
 
-
+    @IBAction func foggy(_ sender: Any) {
+        rootRef.setValue("foggy")
+        changeBackgroundColor(text: "foggy")
+    }
 }
 
